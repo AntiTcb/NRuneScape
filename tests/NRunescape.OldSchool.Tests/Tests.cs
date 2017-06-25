c6b8fa5afdbb3a11b122b4c67ff5c45034245da9
AntiTcb <tcbskater@hotmail.com>
2017-06-24 21:20:00 -0400

    Fix Oldschool.Tests

--- tests/NRunescape.OldSchool.Tests/Tests.cs
+++ tests/NRunescape.OldSchool.Tests/Tests.cs
@@ -1,16 +1,20 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
-using Xunit;                
+using NRuneScape;
+using NRuneScape.OldSchool;
+using Xunit;
 
-namespace NRuneScape.OldSchool.Tests
+namespace NRunescape.OldSchool.Tests
 {
+
     public class Tests : IDisposable
     {
-        internal readonly OSClient _client;
+        internal readonly OldSchoolRestClient _client;
 
         public Tests()
         {
-            _client = new OSClient();
+            _client = new OldSchoolRestClient();
         }
 
         [Fact]
@@ -18,58 +22,75 @@ namespace NRuneScape.OldSchool.Tests
         {
             var account = await _client.GetCharacterAsync("anti-tcb");
             Assert.True(account.Name == "Anti-tcb");
-            Assert.True(account.Agility.Level == 72);
-        }      
+            Assert.True(account.Agility.Level == 73);
+        }
 
         [Fact]
         public async Task GetIronman_IsAntiTcbAndNoIronmanCharacterExists_ReturnsNull()
         {
-            var account = await _client.GetCharacterAsync("anti-tcb", OSGameMode.Ironman);
+            var account = await _client.GetCharacterAsync("anti-tcb", GameMode.Ironman);
             Assert.Null(account);
         }
 
         [Fact]
         public async Task GetHardcoreIronMan_IsAntiTcbAndDoesNotExist_ReturnsNull()
         {
-            var account = await _client.GetCharacterAsync("anti-tcb", OSGameMode.HardcoreIronman);
+            var account = await _client.GetCharacterAsync("anti-tcb", GameMode.HardcoreIronman);
             Assert.Null(account);
         }
 
         [Fact]
         public async Task GetUltimateIronman_IsAntiTcbAndDoesNotExist_ReturnsNull()
         {
-            var account = await _client.GetCharacterAsync("anti-tcb", OSGameMode.UltimateIronman);
+            var account = await _client.GetCharacterAsync("anti-tcb", GameMode.UltimateIronman);
             Assert.Null(account);
         }
 
         [Fact]
         public async Task GetDeadman_IsAntiTcb_ReturnsAccount()
         {
-            var account = await _client.GetCharacterAsync("anti-tcb", OSGameMode.Deadman);
+            var account = await _client.GetCharacterAsync("anti-tcb", GameMode.Deadman);
             Assert.True(account.Overall.Level == 114);
         }
 
         [Fact]
         public async Task GetSeasonal_IsAntiTcb_ReturnsNull()
         {
-            var account = await _client.GetCharacterAsync("anti-tcb", OSGameMode.DeadmanSeasonal);
+            var account = await _client.GetCharacterAsync("anti-tcb", GameMode.DeadmanSeasonal);
             Assert.Null(account);
         }
 
         [Fact]
         public async Task GetRegularSlayer_IsAntiTcb_Returns63()
         {
-            var account = await _client.GetCharacterAsync("antitcb", OSGameMode.Regular);
+            var account = await _client.GetCharacterAsync("antitcb", GameMode.Regular);
             Assert.True(account.Slayer.Level == 63);
         }
 
         [Fact]
         public async Task GetRegularMasterClues_IsAntiTcb_ReturnsNull()
         {
-            var account = await _client.GetCharacterAsync("anti-tcb", OSGameMode.Regular);
+            var account = await _client.GetCharacterAsync("anti-tcb", GameMode.Regular);
             Assert.Null(account.ClueScrollsMaster.Score);
             Assert.Null(account.ClueScrollsMaster.Rank);
-        } 
+        }
+
+        [Fact]
+        public async Task GetAbyssalWhip_ReturnsAbyssalWhip()
+        {
+            var item = await _client.GetItemAsync(4151);
+            Assert.True(item.Name == "Abyssal whip");
+            Assert.True(item.IsMembersItem);
+            Assert.True(item.Description == "A weapon from the abyss.");
+        }
+
+        [Fact]
+        public async Task GetRuneItems_ReturnsCollectionOfItemsBeginningWithRune()
+        {
+            var items = _client.GetItemsAsync("rune", 60);
+            Assert.True(await items.Count() == 60);
+            var item = await _client.GetItemAsync(1319);
+        }
 
         public void Dispose()
         {
